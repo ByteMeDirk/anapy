ab621043f4a584ca067424fbd1ff874442b16017
dirkscgm <dirkscgm@gmail.com>
2020-11-02 11:24:06 +0200

    Workflow Fix - updated setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,55 @@
-import os
-from setuptools import setup
+import re
+import setuptools
 
+with open("README.rst", "r") as file_stream:
+    readme = file_stream.read()
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+with open("requirements.txt", "r") as file_stream:
+    install_requires = file_stream.read().splitlines()
 
+with open("github/__init__.py", "r") as file_stream:
+    version = re.search(r"^__version__ = [\"]([^\"]*)[\"]", file_stream.read(), re.MULTILINE).group(1)
 
-setup(
-    name="anapy",
-    version="0.0.1",
-    author="DirksCGM",
-    author_email="dirkscgm@gmail.com",
-    description="an analytical toolset for working on datasets of various formats",
-    license="MIT",
-    keywords="python, analytics",
-    url="https://github.com/DirksCGM/anapy",
-    install_requires=[
-        'Cython~=0.29.21',
-        'pyyaml~=5.3.1'
-    ],
-    long_description=read('README.md'),
-    python_requires=">=3.6.1"
-)
+if version.endswith(("a", "b", "rc")):
+    try:
+        import subprocess
+
+        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += out.decode("utf-8").strip()
+
+        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += "+g" + out.decode("utf-8").strip()
+    except (Exception) as e:
+        pass
+
+classifiers = [
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: Implementation :: CPython",
+]
+
+project_urls = {}
+
+setuptools.setup(author="DirksCGM",
+                 classifiers=classifiers,
+                 description="ANApy is the beginning of a simplified universal data manipulation library for handling all forms of data",
+                 install_requires=install_requires,
+                 license="Apache Software License",
+                 long_description=readme,
+                 long_description_content_type="text/x-rst",
+                 name="anapy",
+                 python_requires=">=3.6.1",
+                 url="https://github.com/DirksCGM/anapy",
+                 version=version)
