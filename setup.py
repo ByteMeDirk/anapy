d17e5f277f41d711ae9f55492b4539b9ab82cff5
dirkscgm <dirkscgm@gmail.com>
2020-11-04 07:57:39 +0200

    cleaning up for release

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-import re
-
 import setuptools
 
 with open("README.rst", "r") as file_stream:
@@ -7,29 +5,6 @@ with open("README.rst", "r") as file_stream:
 
 with open("requirements.txt", "r") as file_stream:
     install_requires = file_stream.read().splitlines()
-
-with open("github/__init__.py", "r") as file_stream:
-    version = re.search(r"^__version__ = [\"]([^\"]*)[\"]",
-                        file_stream.read(),
-                        re.MULTILINE).group(1)
-
-if version.endswith(("a", "b", "rc")):
-    try:
-        import subprocess
-
-        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"],
-                                   stdout=subprocess.PIPE)
-        out, _ = process.communicate()
-        if out:
-            version += out.decode("utf-8").strip()
-
-        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"],
-                                   stdout=subprocess.PIPE)
-        out, _ = process.communicate()
-        if out:
-            version += "+g" + out.decode("utf-8").strip()
-    except (Exception) as e:
-        pass
 
 classifiers = [
     "Development Status :: 1 - Planning",
@@ -47,16 +22,18 @@ classifiers = [
 
 project_urls = {}
 
-setuptools.setup(author="DirksCGM",
-                 classifiers=classifiers,
-                 description="ANApy is the beginning of a simplified universal "
-                             "data manipulation library for handling all forms "
-                             "of data",
-                 install_requires=install_requires,
-                 license="GNU",
-                 long_description=readme,
-                 long_description_content_type="text/x-rst",
-                 name="anapy",
-                 python_requires=">=3.6.1",
-                 url="https://github.com/DirksCGM/anapy",
-                 version=version)
+setuptools.setup(
+    author="DirksCGM",
+    classifiers=classifiers,
+    description="ANApy is the beginning of a simplified universal "
+                "data manipulation library for handling all forms "
+                "of data",
+    install_requires=install_requires,
+    license="GNU",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    name="anapy",
+    python_requires=">=3.6.1",
+    url="https://github.com/DirksCGM/anapy",
+    version='0.1.0'
+)
